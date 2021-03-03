07b948f165c030ba0fe298810c7a671472ca6e13
BrianSanti <briansanti80@gmail.com>
2021-03-03 08:58:49 -0600

    Programa

---
+++ Prototipo1/Modelo/clsConexion.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class clsConexion
+    {
+        public OdbcConnection conexion()
+        {
+            //creacion de la conexion via ODBC
+            OdbcConnection conn = new OdbcConnection("Dsn=sin");
+            try
+            {
+                conn.Open();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("No Conectó");
+            }
+            return conn;
+        }
+
+        //metodo para cerrar la conexion
+        public void desconexion(OdbcConnection conn)
+        {
+            try
+            {
+                conn.Close();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("No Conectó");
+            }
+        }
+
+
+
+
+
+    }
+}
